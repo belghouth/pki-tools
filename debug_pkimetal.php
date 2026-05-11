486aed1f97a48404114b8dfc5c2a73c53da6a28b
root <tbelghith@gmail.com>
2026-05-11 11:08:11 +0000

    Remove sensitive files, add .gitignore

--- debug_pkimetal.php
+++
@@ -1,8 +0,0 @@
-<?php
-$ch = curl_init('http://127.0.0.1:8080/');
-curl_setopt_array($ch, [CURLOPT_RETURNTRANSFER => true, CURLOPT_TIMEOUT => 3]);
-$out = curl_exec($ch);
-$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-$err = curl_error($ch);
-curl_close($ch);
-echo "HTTP: $code\nError: $err\nBody length: " . strlen($out) . "\n";
